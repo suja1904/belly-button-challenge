17d0536c8e83f1192a91e76b3428251a066558ca
Suja Parekh <sujaaparekh@gmail.com>
2024-05-11 14:08:02 -0700

    git modified
    !#	modified:   static/js/app.js#

--- static/js/app.js
+++ static/js/app.js
@@ -3,19 +3,25 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    var metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
-
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
     // Use d3 to select the panel with id of `#sample-metadata`
+    
+    var PANEL = d3.select("#sample-metadata");
 
 
     // Use `.html("") to clear any existing metadata
 
-
+    PANEL.html("");
+   
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+  });
 
   });
 }
@@ -25,17 +31,42 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    var samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0]
 
     // Get the otu_ids, otu_labels, and sample_values
 
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
     // Build a Bubble Chart
 
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: 'Earth'
+      }
+  }];
 
+  var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      showlegend: false,
+      height: 600,
+      width: 1200,
+      xaxis: { title: 'OTU ID' },
+      hovermode: 'closest'
+  };
+
+  Plotly.newPlot('bubble', bubbleData, bubbleLayout);
     // Render the Bubble Chart
 
 
@@ -43,6 +74,23 @@ function buildCharts(sample) {
 
 
     // Build a Bar Chart
+
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var barData = [{
+            y: yticks,
+            x: sample_values.slice(0, 10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
+        }];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: { t: 30, l: 150 }
+        };
+
+        Plotly.newPlot("bar", barData, barLayout);
+
     // Don't forget to slice and reverse the input data appropriately
 
 
@@ -59,6 +107,15 @@ function init() {
 
 
     // Use d3 to select the dropdown with id of `#selDataset`
+    var select = d3.select("#selDataset");
+    data.names.forEach((name) => {
+        select.append("option").text(name).property("value", name);
+    });
+
+    select.on("change", function () {
+      var newSample = d3.select(this).property("value");
+      optionChanged(newSample);
+  });
 
 
     // Use the list of sample names to populate the select options
@@ -67,18 +124,32 @@ function init() {
 
 
     // Get the first sample from the list
+    var firstSample = data.names[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+    // Initialize the page with the first sample for gauge chart
+    buildGaugeChart(firstSample); 
+});
 
 
     // Build charts and metadata panel with the first sample
 
-  });
-}
+
+
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
+  function optionChanged(newSample) {
+    console.log("New sample selected:", newSample);
+    buildCharts(newSample);
+    buildMetadata(newSample);
+    buildGaugeChart(newSample); 
+}
 
 }
 
 // Initialize the dashboard
 init();
+
+}
